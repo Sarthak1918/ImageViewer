50416fa3c7d4e2d9a6cff4c3540bb275cda5a18f
Sarthak1918 <78781587+Sarthak1918@users.noreply.github.com>
2022-11-07 22:29:50 +0530

    code update

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'typeface-roboto';
+import * as serviceWorker from './serviceWorker';
+import Controller from './screens/Controller';
+
+ReactDOM.render(<Controller/>, document.getElementById('root'));
+
+serviceWorker.unregister();
